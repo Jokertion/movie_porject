23585b51fca8f5d312aa498e58bf67e1c43ea9ea
Miracle_lms <37183438+Jokertion@users.noreply.github.com>
2018-09-05 10:42:50 +0800

    初始化文件

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
-#coding:utf8
+# coding:utf8
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 app.debug = True
@@ -10,3 +11,7 @@ from app.admin import admin as admin_blueprint
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix="/admin")
 
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("home/404.html"), 404
